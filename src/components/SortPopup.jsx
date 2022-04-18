e53874f1e063040125d8bf9d994f6f733105158b
TryGik <ahutinon@gmail.com>
2022-04-18 16:01:08 +0300

    starting routing

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -33,7 +33,7 @@ export default function SortPopup({ items }) {
     return (
         <div className="sort" ref={sortRef}>
             <div className="sort__label">
-                <svg
+                <svg className={visiblePopup ? 'rotated' : ''}
                     width="10"
                     height="6"
                     viewBox="0 0 10 6"
